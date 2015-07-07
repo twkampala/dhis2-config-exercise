a06d70e33cd4662a9b0ba324fbfa10ed324cfd5e
Remo Samuel Paul <remosam42@gmail.com>
2015-07-07 11:53:01 +0530

    [Remo, MJ] Added generated data elements to the two sections

--- setup_datasets.py
+++ setup_datasets.py
@@ -21,9 +21,45 @@ class DataSetMetaDataGenerator(object):
             'id': self.create_id(dataset_name),
             'periodType':'Weekly',
         }
+        return [dataset]
+    
+    def build_data_elements(self, prefix, count = 10):
+        elements = []
+        for n in range(10):
+            name = '%s %d data element' % (prefix, n)
+            elements.append({'name': name, 
+                             'id': self.create_id(name),
+                             'shortName': name,
+                             'code': name,
+                             'domainType': 'AGGREGATE',
+                             'aggregationOperator': 'sum',
+                             'type': 'int'})
+        return elements
+    
+    def create_sections(self, dataset_id):
+        first_section_data_elements = self.build_data_elements('First')
+        second_section_data_elements = self.build_data_elements('Second')
+        section_info = [{'name':'first section', 'elements': first_section_data_elements }, 
+                        {'name':'second section', 'elements': second_section_data_elements}]
+        sections = []
+        for item in section_info:
+            name = item['name']
+            sections.append({
+                'name':name,
+                'id':self.create_id(name),
+                'displayName': name,
+                'dataSet': {'id': dataset_id},
+                'dataElements': item['elements']
+            })
+        return sections, first_section_data_elements + second_section_data_elements
 
+    def run(self):
+        datasets = self.create_dataset()
+        sections, data_elements = self.create_sections(datasets[0]['id'])
         metadata = {
-            'dataSets': [dataset]
+            'dataSets': datasets,
+            'sections': sections,
+            'dataElements': data_elements
         }
 
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
@@ -32,8 +68,6 @@ class DataSetMetaDataGenerator(object):
 
         print response.content
 
-    def run(self):
-        self.create_dataset()
 
 
 def main():
